7a4472ee55147de25c078bec35711da8200e15e7
Yunusbek Sadullayev <iam.developer.yu@gmail.com>
2025-07-11 05:48:45 +0500

    learn strong foundation to create app

---
+++ PetSave/PetSaveTests/Tests/Core/Data/CoreData/PetSaveTests.swift
@@ -0,0 +1,16 @@
+//
+//  PetSaveTests.swift
+//  PetSaveTests
+//
+//  Created by Sarvar Boltaboyev on 11/07/25.
+//
+
+import Testing
+
+struct PetSaveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
